1a9000051afa6743aeb0ed0b41ffa938ba281190
Teo <teo.lairla@iessierradeguara.com>
2021-10-26 12:13:30 +0200

    plantilla prog08

---
+++ prog08-forks-y-senyales/main.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+//definimos el manejador de la señal
+void manejadorHijo(int num_senyal);
+void manejadorPadre(int num_senyal);
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
+
+void manejadorHijo(int num_senyal){
+
+}
+void manejadorPadre(int num_senyal){
+    
+};
