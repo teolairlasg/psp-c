8bdc523f9e85357f7e899fb28d08b1f5b4cf24e9
Teo <teo.lairla@iessierradeguara.com>
2021-11-02 10:29:07 +0100

    AÑADIDO ;

--- prog08-forks-y-senyales/main.c
+++ prog08-forks-y-senyales/main.c
@@ -1,21 +1,39 @@
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <signal.h>
+#include <unistd.h>  //define la función fork
+#include <sys/types.h> //define el tipo pid_t
+#include <sys/wait.h> //define la función wait y waitpid
+#include <signal.h> //define la función signal y los códigos SIGUSR1...
+#include <stdio.h>  //define la función printf
+#include <stdlib.h>  //define la función exit
 
-//definimos el manejador de la señal
+//declaramos el manejador de la señal
 void manejadorHijo(int num_senyal);
 void manejadorPadre(int num_senyal);
 
 int main(int argc, char const *argv[])
 {
-    
+    pid_t pid;
+    pid = fork();
+    if (pid == -1){ //gestionamos un posible error
+        perror("Error en el fork");
+        exit(1);
+    }
+    if(pid == 0){ //Soy el proceso hijo
+        signal(SIGUSR1, manejadorHijo);
+        printf("[Proceso: %d]: Manejador activado.\n",getpid());
+        pause();
+    }else{ //soy el padre
+        signal(SIGUSR1, manejadorPadre);
+        printf("[Proceso: %d]: Manejador activado.\n",getpid());
+        pause();
+        pid_t pid_hijo = wait(NULL);
+    }
     return 0;
 }
 
+//implementamos los manejadores
 void manejadorHijo(int num_senyal){
-
+    printf("Soy el manejador de la señal del hijo.\n");
 }
 void manejadorPadre(int num_senyal){
-    
+    printf("Soy el manejador de la señal del padre.\n");
 };
